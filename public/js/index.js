02b6cf3eee1ad4e267718aeae2e3a8bfb2ab16df
tetramer <adjklx@gmail.com>
2013-05-31 17:34:21 -0400

    three.js cube example

---
+++ public/js/index.js
@@ -0,0 +1,25 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+
+
+var geometry = new THREE.CubeGeometry(1,1,1);
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+
+
+function render() {
+	requestAnimationFrame(render);
+	cube.rotation.x += 0.1;
+	cube.rotation.y += 0.1;
+	renderer.render(scene, camera);
+}
+render();

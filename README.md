ffdc6bb84317e7b52ec302cb7d7739d1e9d50b67
tetramer <adjklx@gmail.com>
2013-05-31 23:01:59 -0400

    spheres; lighting; first person controls

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 README File
 Repo for Josh and Chris' super secret space project
-zxcv-z-cv-zx-zxc
--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,96 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+(function ($, THREE) {
+	var config, $window, width, height, scene, camera, renderer, sphere;
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+	// constants
+	config = {
+		'scale': 123,
+		'moveSpeed': 0.0001,
+		'lookSpeed': 0.1
+	};
+
+	// dimensions
+	$window = $(window);
+	width = $window.width();
+	height = $window.height() - 5;
+
+	// clock: for movement speed
+	clock = new THREE.Clock();
+
+	// scene
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(90, width / height, 0.1, 100);
+	camera = new THREE.OrthographicCamera(-width / config.scale, width / config.scale, height / config.scale, -height / config.scale, 0.1, 100);
+
+	// canvas
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(width, height);
+	$('body').append(renderer.domElement);
 
 
 
-var geometry = new THREE.CubeGeometry(1,1,1);
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+	// construct objects
+	sphere = new THREE.Mesh(
+		new THREE.SphereGeometry(3, 32, 32),
+		new THREE.MeshLambertMaterial({
+			'color': 0xcc8844,
+			'wireframe': true
+		})
+	);
+	sphere.position.set(-4, 3, 6);
+	scene.add(sphere);
 
-camera.position.z = 5;
+	sphere2 = new THREE.Mesh(
+		new THREE.SphereGeometry(6, 32, 32),
+		new THREE.MeshPhongMaterial({
+			'color': 0x4488cc,
+			'wireframe': true
+		})
+	);
+	sphere2.position.set(10, 0, 6);
+	scene.add(sphere2);
 
 
 
-function render() {
-	requestAnimationFrame(render);
-	cube.rotation.x += 0.1;
-	cube.rotation.y += 0.1;
-	renderer.render(scene, camera);
-}
-render();
+	// light
+	light = new THREE.HemisphereLight(0xffffff, 0x000000, 1);
+	light = new THREE.PointLight(0x8888cc, 10, 15);
+	light.position.set(2, 2, 3);
+	scene.add(light);
+
+
+
+	camera.lookAt(new THREE.Vector3(1, 2, 3));
+
+
+
+	// first person controls
+	controls = new THREE.FirstPersonControls(camera);
+	controls.movementSpeed = config.movementSpeed;
+	controls.lookSpeed = config.lookSpeed;
+
+	// render
+	function render() {
+		window.requestAnimationFrame(render);
+
+		controls.update(clock.getDelta());
+
+		sphere.rotation.x += 0.01;
+		sphere.rotation.y += 0.02;
+		sphere2.rotation.x -= 0.005;
+		sphere2.rotation.y += 0.002;
+
+		renderer.render(scene, camera);
+	}
+	render();
+
+	$(function () {
+		$window.on({
+			'focus': function() {
+				if (controls) controls.freeze = false;
+			},
+			'blur': function() {
+				if (controls) controls.freeze = true;
+			}
+		});
+	});
+}(jQuery, THREE));

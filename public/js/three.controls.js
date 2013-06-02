5febf37e9799585bc36454db02ac993eaf315112
tetramer <adjklx@gmail.com>
2013-06-02 18:18:43 -0400

    a

--- public/js/three.controls.js
+++ public/js/three.controls.js
@@ -3,173 +3,150 @@
  * Based on PointerLockControls.js. Edited by Chris.
  */
 
-THREE.Controls = function (camera, options) {
-	options = $.extend({
-		'lookSpeed': 0.001,
-		'moveSpeed': 100,
-		'strafeSpeed': 100,
-		'rollSpeed': 1
-	}, options);
+(function ($, THREE) {
+	THREE.Controls = function (camera, options) {
+		options = $.extend({
+			'lookSpeed': 0.001,
+			'moveSpeed': 100,
+			'strafeSpeed': 100,
+			'rollSpeed': 1
+		}, options);
 
-	var self = this;
+		var self, states, events, $document;
 
-	var moveForward = false;
-	var moveBackward = false;
-	var moveLeft = false;
-	var moveRight = false;
-	var moveUp = false;
-	var moveDown = false;
-	var rollLeft = false;
-	var rollRight = false;
+		self = this;
 
-	var translationVector = new THREE.Vector3();
+		states = {
+			'moveForward': false,
+			'moveBackward': false,
+			'moveLeft': false,
+			'moveRight': false,
+			'moveUp': false,
+			'moveDown': false,
+			'rollLeft': false,
+			'rollRight': false
+		};
 
-	var $document = $(document);
+		$document = $(document);
 
-	// unit vectors
-	var i, j, k;
-	i = new THREE.Vector3(1, 0, 0);
-	j = new THREE.Vector3(0, 1, 0);
-	k = new THREE.Vector3(0, 0, 1);
-
-	var onMouseMove = function (event) {
-		if (!self.enabled) {
-			return;
+		// unit vectors
+		if (typeof THREE.unitVectors !== 'object') {
+			THREE.unitVectors = {
+				'i': new THREE.Vector3(1, 0, 0),
+				'j': new THREE.Vector3(0, 1, 0),
+				'k': new THREE.Vector3(0, 0, 1)
+			};
 		}
 
-		var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
-		var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
-console.log('moving');
-		camera.rotateOnAxis(j, -movementX * options.lookSpeed);
-		camera.rotateOnAxis(i, -movementY * options.lookSpeed);
-	};
+		events = {
+			'mousemove': function (event) {
+				var movementX, movementY;
 
-	var onKeyDown = function (event) {
-		switch (event.keyCode) {
-			case 38: // up
-			case 87: // w
-				moveForward = true;
-				break;
+				if (!self.enabled) {
+					return;
+				}
 
-			case 37: // left
-			case 65: // a
-				moveLeft = true;
-				break;
+				movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
+				movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
 
-			case 40: // down
-			case 83: // s
-				moveBackward = true;
-				break;
+				camera.rotateOnAxis(THREE.unitVectors.j, -movementX * options.lookSpeed);
+				camera.rotateOnAxis(THREE.unitVectors.i, -movementY * options.lookSpeed);
+			},
+			'keydown': function (event) {
+				switch (event.which) {
+					case 38: case 87: states.moveForward = true; break; // up, W
+					case 40: case 83: states.moveBackward = true; break; // down, S
+					case 37: case 65: states.moveLeft = true; break; // left, A
+					case 39: case 68: states.moveRight = true; break; // right, D
+					case 82: states.moveUp = true; break; // R
+					case 70: states.moveDown = true; break; // F
+					case 81: states.rollLeft = true; break; // Q
+					case 69: states.rollRight = true; break; // E
+				}
+			},
+			'keyup': function (event) {
+				switch(event.which) {
+					case 38: case 87: states.moveForward = false; break; // up, W
+					case 40: case 83: states.moveBackward = false; break; // down, S
+					case 37: case 65: states.moveLeft = false; break; // left, A
+					case 39: case 68: states.moveRight = false; break; // right, D
+					case 82: states.moveUp = false; break; // R
+					case 70: states.moveDown = false; break; // F
+					case 81: states.rollLeft = false; break; // Q
+					case 69: states.rollRight = false; break; // E
+				}
+			}
+		};
 
-			case 39: // right
-			case 68: // d
-				moveRight = true;
-				break;
-
-			case 82: /*R*/ moveUp = true; break;
-			case 70: /*F*/ moveDown = true; break;
-
-			case 81: /*Q*/ rollLeft = true; break;
-			case 69: /*E*/ rollRight = true; break;
-		}
-	};
-
-	var onKeyUp = function (event) {
-		switch(event.keyCode) {
-			case 38: // up
-			case 87: // w
-				moveForward = false;
-				break;
-
-			case 37: // left
-			case 65: // a
-				moveLeft = false;
-				break;
-
-			case 40: // down
-			case 83: // a
-				moveBackward = false;
-				break;
-
-			case 39: // right
-			case 68: // d
-				moveRight = false;
-				break;
-
-			case 82: /*R*/ moveUp = false; break;
-			case 70: /*F*/ moveDown = false; break;
-
-			case 81: /*Q*/ rollLeft = false; break;
-			case 69: /*E*/ rollRight = false; break;
-		}
-	};
-
-	// enable/disable
-	this.enabled = false;
-	this.enable = function () {
-		$document.on({
-			'keydown': onKeyDown,
-			'keyup': onKeyUp
-		});
-
-		// not using jQuery to get event.movementX properties
-		document.addEventListener('mousemove', onMouseMove, false);
-
-		this.enabled = true;
-	};
-	this.disable = function () {
-		$document.off({
-			'mousemove': onMouseMove,
-			'keydown': onKeyDown,
-			'keyup': onKeyUp
-		});
+		// enable/disable
 		this.enabled = false;
+		this.enable = function () {
+			$document.on({
+				'keydown': events.keydown,
+				'keyup': events.keyup
+			});
+
+			// not using jQuery to get event.movementX properties
+			document.addEventListener('mousemove', events.mousemove, false);
+
+			this.enabled = true;
+		};
+		this.disable = function () {
+			$document.off({
+				'mousemove': events.mousemove,
+				'keydown': events.keydown,
+				'keyup': events.keyup
+			});
+			this.enabled = false;
+		};
+
+		this.update = function (delta) {
+			var translationVector = new THREE.Vector3();
+
+			if (!self.enabled) {
+				return;
+			}
+
+			// translate
+			if (states.moveForward) translationVector.z -= delta * options.moveSpeed;
+			if (states.moveBackward) translationVector.z += delta * options.moveSpeed;
+
+			if (states.moveLeft) translationVector.x -= delta * options.strafeSpeed;
+			if (states.moveRight) translationVector.x += delta * options.strafeSpeed;
+
+			if (states.moveUp) translationVector.y += delta * options.strafeSpeed;
+			if (states.moveDown) translationVector.y -= delta * options.strafeSpeed;
+
+			camera.translateX(translationVector.x);
+			camera.translateY(translationVector.y);
+			camera.translateZ(translationVector.z);
+
+
+			// rotate
+			if (states.rollLeft) {
+				camera.rotateOnAxis(THREE.unitVectors.k, -delta * options.rollSpeed);
+			}
+			if (states.rollRight) {
+				camera.rotateOnAxis(THREE.unitVectors.k, delta * options.rollSpeed);
+			}
+
+
+			$('#hud2').html(
+				'position (px): ' +
+					Math.round(camera.position.x) + ', ' +
+					Math.round(camera.position.y) + ', ' +
+					Math.round(camera.position.z) + '<br />' +
+				'rotation (deg): ' +
+					Math.round(camera.rotation.x * 180 / Math.PI) + ', ' +
+					Math.round(camera.rotation.y * 180 / Math.PI) + ', ' +
+					Math.round(camera.rotation.z * 180 / Math.PI) + '<br />' +
+				'movement vector (px): ' +
+					Math.round(translationVector.x) + ', ' +
+					Math.round(translationVector.y) + ', ' +
+					Math.round(translationVector.z)
+			);
+
+			translationVector.set(0, 0, 0);
+		};
 	};
-
-	this.update = function (delta) {
-		if (!self.enabled) {
-			return;
-		}
-
-		// translate
-		if (moveForward) translationVector.z -= delta * options.moveSpeed;
-		if (moveBackward) translationVector.z += delta * options.moveSpeed;
-
-		if (moveLeft) translationVector.x -= delta * options.strafeSpeed;
-		if (moveRight) translationVector.x += delta * options.strafeSpeed;
-
-		if (moveUp) translationVector.y += delta * options.strafeSpeed;
-		if (moveDown) translationVector.y -= delta * options.strafeSpeed;
-
-		camera.translateX(translationVector.x);
-		camera.translateY(translationVector.y);
-		camera.translateZ(translationVector.z);
-
-
-		// rotate
-		if (rollLeft) {
-			camera.rotateOnAxis(k, -delta * options.rollSpeed);
-		}
-		if (rollRight) {
-			camera.rotateOnAxis(k, delta * options.rollSpeed);
-		}
-
-
-		$('#hud2').html(
-			'position (px): ' +
-				Math.round(camera.position.x) + ', ' +
-				Math.round(camera.position.y) + ', ' +
-				Math.round(camera.position.z) + '<br />' +
-			'rotation (deg): ' +
-				Math.round(camera.rotation.x * 180 / Math.PI) + ', ' +
-				Math.round(camera.rotation.y * 180 / Math.PI) + ', ' +
-				Math.round(camera.rotation.z * 180 / Math.PI) + '<br />' +
-			'movement vector (px): ' +
-				Math.round(translationVector.x) + ', ' +
-				Math.round(translationVector.y) + ', ' +
-				Math.round(translationVector.z)
-		);
-		console.log('q');
-		translationVector.set(0, 0, 0);
-	};
-};
+}(jQuery, THREE));

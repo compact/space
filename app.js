2bc81b74b7b4c9ba8f2686bef1b540d705c32c4d
joshacheson <joshuaacheson@Joshuas-MacBook.local>
2013-06-14 15:05:00 -0400

    test again

--- app.js
+++ app.js
@@ -42,5 +42,5 @@ app.get('/', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
-  console.log('pushtest');
+  console.log('pushtest2');
 });

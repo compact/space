233b7a6402aa5b6fdd6a890e66c53cd4ffe2b12c
joshacheson <joshuaacheson@Joshuas-MacBook.local>
2013-06-14 00:27:00 -0400

    test git to deploy

--- app.js
+++ app.js
@@ -40,7 +40,6 @@ db.once('open', function callback () {
 
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));

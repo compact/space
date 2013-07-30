3480babf1a9a01c1f0da71cb3942d2c171534d53
compact <adjklx@gmail.com>
2013-07-30 10:54:36 -0400

    refactor into prototype object

--- public/js/three.controls.js
+++ public/js/three.controls.js
@@ -8,16 +8,15 @@
 	'use strict';
 
 	THREE.Controls = function (camera, options) {
-		options = $.extend({
+		var self = this;
+
+		this.camera = camera;
+		this.options = $.extend({
 			'lookSpeed': 0.00025,
 			'zSpeed': 500,
 			'strafeSpeed': 500,
 			'rollSpeed': 2
 		}, options);
-
-		var self, events, $document, resetStates;
-
-		self = this;
 
 /* used in PointerLockControls, but doesn't seem necessary
 		yawObject takes the place of the camera object
@@ -28,24 +27,11 @@
 		scene.add(yawObject);
 */
 
-		$document = $(document);
+		this.resetStates();
+		this.$document = $(document);
 
-		resetStates = function () {
-			self.states = {
-				'moveForward': false,
-				'moveBackward': false,
-				'moveLeft': false,
-				'moveRight': false,
-				'moveUp': false,
-				'moveDown': false,
-				'rollLeft': false,
-				'rollRight': false
-			};
-		};
-
-		resetStates();
-
-		events = {
+		// event handlers; not a propotype property for convenient reference to self
+		this.events = {
 			'mousemove': function (event) {
 				var movementX, movementY;
 
@@ -57,8 +43,8 @@
 				movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
 
 				// TODO perform the rotation in update() instead of here -Chris
-				camera.rotateOnAxis(THREE.unitVectors.x, -movementY * options.lookSpeed);
-				camera.rotateOnAxis(THREE.unitVectors.y, -movementX * options.lookSpeed);
+				self.camera.rotateOnAxis(THREE.unitVectors.x, -movementY * self.options.lookSpeed);
+				self.camera.rotateOnAxis(THREE.unitVectors.y, -movementX * self.options.lookSpeed);
 			},
 			'keydown': function (event) {
 				switch (event.which) {
@@ -85,91 +71,104 @@
 				}
 			}
 		};
+	};
 
-		// enable/disable
+	THREE.Controls.prototype.resetStates = function () {
+		this.states = {
+			'moveForward': false,
+			'moveBackward': false,
+			'moveLeft': false,
+			'moveRight': false,
+			'moveUp': false,
+			'moveDown': false,
+			'rollLeft': false,
+			'rollRight': false
+		};
+	};
+
+	// enable/disable
+	THREE.Controls.prototype.enabled = false;
+	THREE.Controls.prototype.enable = function () {
+		this.$document.on({
+			'keydown': this.events.keydown,
+			'keyup': this.events.keyup
+		});
+
+		// not using jQuery to get event.movementX properties
+		document.addEventListener('mousemove', this.events.mousemove, false);
+
+		this.enabled = true;
+	};
+	THREE.Controls.prototype.disable = function () {
+		this.resetStates();
+		this.$document.off({
+			'mousemove': this.events.mousemove,
+			'keydown': this.events.keydown,
+			'keyup': this.events.keyup
+		});
 		this.enabled = false;
-		this.enable = function () {
-			$document.on({
-				'keydown': events.keydown,
-				'keyup': events.keyup
-			});
+	};
 
-			// not using jQuery to get event.movementX properties
-			document.addEventListener('mousemove', events.mousemove, false);
+	// speedMultiplier is delta, applies to all movements (translations and
+	// rotations
+	THREE.Controls.prototype.moveCamera = function (speedMultiplier, translationSpeedMultiplier) {
+		var translationVector = new THREE.Vector3(), angle = 0;
 
-			this.enabled = true;
-		};
-		this.disable = function () {
-			resetStates();
-			$document.off({
-				'mousemove': events.mousemove,
-				'keydown': events.keydown,
-				'keyup': events.keyup
-			});
-			this.enabled = false;
-		};
+		if (!this.enabled) {
+			return;
+		}
 
-		// speedMultiplier is delta, applies to all movements (translations and
-		// rotations
-		this.moveCamera = function (speedMultiplier, translationSpeedMultiplier) {
-			var translationVector = new THREE.Vector3(), angle = 0;
+		// translate
+		var translationVector = this.getLocalTranslationVector();
+		this.camera.translateX(translationVector.x * this.options.strafeSpeed *
+			speedMultiplier * translationSpeedMultiplier);
+		this.camera.translateY(translationVector.y * this.options.strafeSpeed *
+			speedMultiplier * translationSpeedMultiplier);
+		this.camera.translateZ(translationVector.z * this.options.zSpeed *
+			speedMultiplier * translationSpeedMultiplier);
 
-			if (!self.enabled) {
-				return;
-			}
+		// rotate
+		if (this.states.rollLeft) {
+			angle -= this.options.rollSpeed * speedMultiplier;
+		}
+		if (this.states.rollRight) {
+			angle += this.options.rollSpeed * speedMultiplier;
+		}
+		this.camera.rotateOnAxis(THREE.unitVectors.z, angle);
+	};
 
-			// translate
-			var translationVector = this.getLocalTranslationVector();
-			camera.translateX(translationVector.x * options.strafeSpeed *
-				speedMultiplier * translationSpeedMultiplier);
-			camera.translateY(translationVector.y * options.strafeSpeed *
-				speedMultiplier * translationSpeedMultiplier);
-			camera.translateZ(translationVector.z * options.zSpeed *
-				speedMultiplier * translationSpeedMultiplier);
+	// Return a Vector3 object corresponding to the current local movement
+	// direction(s). To check whether the camera is currently moving, call
+	// .getTranslationVector().length() > 0
+	THREE.Controls.prototype.getLocalTranslationVector = function () {
+		var vector = new THREE.Vector3();
 
-			// rotate
-			if (self.states.rollLeft) {
-				angle -= options.rollSpeed * speedMultiplier;
-			}
-			if (self.states.rollRight) {
-				angle += options.rollSpeed * speedMultiplier;
-			}
-			camera.rotateOnAxis(THREE.unitVectors.z, angle);
-		};
-
-		// Return a Vector3 object corresponding to the current local movement
-		// direction(s). To check whether the camera is currently moving, call
-		// .getTranslationVector().length() > 0
-		this.getLocalTranslationVector = function () {
-			var vector = new THREE.Vector3();
-
-			if (!self.enabled) {
-				return vector;
-			}
-
-			if (self.states.moveForward) {
-				vector.add(THREE.unitVectors.negZ);
-			}
-			if (self.states.moveBackward) {
-				vector.add(THREE.unitVectors.z);
-			}
-			if (self.states.moveLeft) {
-				vector.add(THREE.unitVectors.negX);
-			}
-			if (self.states.moveRight) {
-				vector.add(THREE.unitVectors.x);
-			}
-			if (self.states.moveUp) {
-				vector.add(THREE.unitVectors.y);
-			}
-			if (self.states.moveDown) {
-				vector.add(THREE.unitVectors.negY);
-			}
+		if (!this.enabled) {
 			return vector;
-		};
-		// Not used.
-		this.getWorldTranslationVector = function () {
-			return camera.localToWorld(this.getLocalTranslationVector());
-		};
+		}
+
+		if (this.states.moveForward) {
+			vector.add(THREE.unitVectors.negZ);
+		}
+		if (this.states.moveBackward) {
+			vector.add(THREE.unitVectors.z);
+		}
+		if (this.states.moveLeft) {
+			vector.add(THREE.unitVectors.negX);
+		}
+		if (this.states.moveRight) {
+			vector.add(THREE.unitVectors.x);
+		}
+		if (this.states.moveUp) {
+			vector.add(THREE.unitVectors.y);
+		}
+		if (this.states.moveDown) {
+			vector.add(THREE.unitVectors.negY);
+		}
+		return vector;
+	};
+	// Not used.
+	THREE.Controls.prototype.getWorldTranslationVector = function () {
+		return this.camera.localToWorld(this.getLocalTranslationVector());
 	};
 }(jQuery, THREE));

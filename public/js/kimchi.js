f77e4c5e21e39247a62968ffa2bb9d4474fa4dab
lambda <adjklx@gmail.com>
2013-09-02 23:12:07 -0400

    move js to end of body; auto init kimchi again

--- public/js/kimchi.js
+++ public/js/kimchi.js
@@ -6,20 +6,19 @@
     KIMCHI.$window = $(window);
     KIMCHI.$overlay = $('#overlay');
 
+    KIMCHI.init();
+
     KIMCHI.$overlay.one('click', '.continue-flying', function () {
       var $this = $(this);
 
-      $this.button('loading');
-      // short delay for the button to go into its loading state first
+      KIMCHI.pointerLock.request(); // async
+      // TODO: this button changing before free flight gets enabled is unsightly, so move this line to pointerLock change()
       setTimeout(function () {
-        KIMCHI.init();
-        KIMCHI.pointerLock.request(); // async
-        // TODO: this button changing before free flight gets enabled is unsightly, so move this line to pointerLock change()
         $this.button('continue');
+      }, 250);
 
-        KIMCHI.$overlay.on('click', '.continue-flying',
-          KIMCHI.pointerLock.request);
-      }, 50);
+      KIMCHI.$overlay.on('click', '.continue-flying',
+        KIMCHI.pointerLock.request);
     });
 
   });

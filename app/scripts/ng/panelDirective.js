7425711ed0dece33979a9cce587e563d59ca359d
lambda <adjklx@gmail.com>
2013-10-22 17:48:13 -0400

    style panels and panels menu

--- app/scripts/ng/panelDirective.js
+++ app/scripts/ng/panelDirective.js
@@ -4,7 +4,7 @@ app.directive('panel', function () {
     'transclude': true,
     'scope': true,
     'template':
-      '<div class="panel panel-info" ng-show="panels[key]" stop-propagation="mousedown">' +
+      '<div class="panel panel-success" ng-show="panels[key]" stop-propagation="mousedown">' +
         '<div class="panel-heading">' +
           '<button type="button" class="close" aria-hidden="true" ng-click="panels[key] = false">&times;</button>' +
           '<h2 class="panel-title">{{title}}</h3>' +

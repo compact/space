f4aa2799854e52936383162e00905453a2224a47
compact <adjklx@gmail.com>
2014-03-23 03:20:41 -0400

    labels: set z-index depending on relative distance

--- app/scripts/ng/LabelsCtrl.js
+++ app/scripts/ng/LabelsCtrl.js
@@ -1,14 +1,21 @@
 angular.module('kimchi').controller('LabelsCtrl', function ($scope, KIMCHI) {
   KIMCHI.on('render', function () {
+    var sortedDistances = KIMCHI.space.getSortedDistances();
+
     $scope.labels = [];
+
     _.each(KIMCHI.space.bodies, function (body) {
       var coords = body.getCanvasCoords();
 
       if (coords) {
         $scope.labels.push({
           'name': body.name,
-          'left': coords.x + 'px',
-          'top': coords.y + 'px'
+          'css': {
+            'left': coords.x + 'px',
+            'top': coords.y + 'px',
+            // TODO: constant offset
+            'zIndex': 100 + _.findIndex(sortedDistances, {'name': body.name})
+          }
         });
       }
     });

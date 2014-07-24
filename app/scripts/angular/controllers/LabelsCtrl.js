84cc587068dd047e465387e9f13c2f3b7624d710
compact <adjklx@gmail.com>
2014-07-24 04:43:29 -0400

    change the labels to always exist rather than created every frame so they can be hovered properly

--- app/scripts/angular/controllers/LabelsCtrl.js
+++ app/scripts/angular/controllers/LabelsCtrl.js
@@ -1,23 +1,36 @@
 angular.module('kimchi').controller('LabelsCtrl', function ($scope, KIMCHI) {
-  KIMCHI.on('render', function () {
-    $scope.labels = [];
+  $scope.labels = {};
 
+  // initialize the labels with the relevant Body properties
+  KIMCHI.init.promise.then(function () {
+    _.each(KIMCHI.space.bodies, function (body) {
+      $scope.labels[body.name] = {
+        'name': body.name,
+        'css': {}
+      };
+    });
+  });
+
+  // update the labels on every render
+  KIMCHI.on('render', function () {
     if (KIMCHI.config.get('showLabels')) {
-      var sortedDistances = KIMCHI.space.getSortedDistances();
+      var label, coords, sortedDistances = KIMCHI.space.getSortedDistances();
 
       _.each(KIMCHI.space.bodies, function (body) {
-        var coords = body.getCanvasCoords();
+        label = $scope.labels[body.name];
+        coords = body.getCanvasCoords();
 
         if (coords) {
-          $scope.labels.push({
-            'name': body.name,
-            'css': {
-              'left': coords.x + 'px',
-              'top': coords.y + 'px',
-              // TODO: constant offset
-              'zIndex': 100 + _.findIndex(sortedDistances, {'name': body.name})
-            }
-          });
+          // show and position the label
+          label.css.left = coords.x + 'px',
+          label.css.top = coords.y + 'px',
+          label.css.zIndex = 100 + // TODO: constant offset
+            _.findIndex(sortedDistances, {'name': body.name});
+          label.show = true;
+        } else {
+          // hide the label; the Body is either not visible in the current view
+          // or too far away
+          label.show = false;
         }
       });
     }
